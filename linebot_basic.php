d4970bfb8a871603284a10fd8cb2d7ac563fc99a
Thanach Jintakanont <68634580+earth007zamag@users.noreply.github.com>
2020-11-17 23:23:54 +0800

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -26,12 +26,12 @@ foreach ($request_json['events'] as $event)
 					$result_json = mySQL_selectAll('http://bot.kantit.com/json_select_users.php');
 					foreach($result_json as $values) {
 						if($txts[2] == $values["user_firstname"]){
-							$data .= $values["user_firstname"] . " " . $values["user_lastname"] . "\r\n";
+							$data = $values["user_firstname"] . " " . $values["user_lastname"] . "\r\n";
 						}
 					}
+					$reply_message = $data;
 				}
 			}
- 			$reply_message = $data;
 			
 			
 		} else {
