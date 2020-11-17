ea7fbb61416ecddad27fc91f6449fea59000aa49
Thanach Jintakanont <68634580+earth007zamag@users.noreply.github.com>
2020-11-17 23:55:35 +0800

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -86,7 +86,7 @@ function mySQL_selectAll($url)
 	$data = "ผลลัพธ์:\r\n";
 		
 	foreach($result_json as $values) {
-		$data .= $values["stuid"] . " " . $values["fullname"] . "\r\n";
+		$data .= $values["user_firstname"] . " " . $values["user_lastname"] . "\r\n";
 	}
 	
 	return $data;
